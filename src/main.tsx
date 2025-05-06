76753f177ee35f60fa3babf7017bdf2ea336ab72
채정욱 <jeonguk95@gmail.com>
2025-05-06 11:52:20 +0900

    Cnvkwer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// ✅ 반드시 이 줄 있어야 Tailwind 적용됨
+// 반드시 이 줄이 있어야 TailwindCSS 클래스가 적용됩니다
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
